ccb1d2eeb152606d568abafc657330fd75401703
ialoysious <isaacaloysious@gmail.com>
2023-05-01 14:32:16 -0700

    login/signup

--- ecomm_front/pages/login.js
+++ ecomm_front/pages/login.js
@@ -96,7 +96,7 @@ const { csrfToken } = useContext(Csrf_context);
 								</Link>
 							</Grid>
 							<Grid item>
-								<Link href='#' variant='body2'>
+								<Link href='/signup' variant='body2'>
 									{"Don't have an account? Sign Up"}
 								</Link>
 							</Grid>
--- ecomm_front/pages/signup.js
+++ ecomm_front/pages/signup.js
@@ -16,6 +16,7 @@ import { Csrf_context } from '@/context/csrf_context';
 
 
 
+
 export default function Signup() {
     const signUpNewUser = async (userData) => {
         const response = await fetch('http://localhost:8000/users/register/', {
@@ -53,115 +54,117 @@ export default function Signup() {
     const [error, setError] = useState('');
     const { csrfToken } = useContext(Csrf_context);
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div>
-        <Avatar>
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form onSubmit={handleSubmit} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete="username"
-                name="username"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                autoFocus
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <TextField
-                    autoComplete="password"
-                    name="password"
-                    required
-                    fullWidth
-                    id="password"
-                    label="Password"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <TextField
-                    autoComplete="email"
-                    name="email"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete="street-address"
-                name="streetAddress"
-                required
-                fullWidth
-                id="streetAddress"
-                label="Street Address"
-                value={streetAddress}
-                onChange={(e) => setStreetAddress(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="city"
-                name="city"
-                required
-                fullWidth
-                id="city"
-                label="City"
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="state"
-                name="state"
-                required
-                fullWidth
-                id="state"
-                label="State"
-                value={state}
-                onChange={(e) => setState(e.target.value)}
-                />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <TextField
-                    autoComplete="zip-code"
-                    name="zipCode"
-                    required
-                    fullWidth
-                    id="zipCode"
-                    label="Zip Code"
-                    value={zipCode}
-                    onChange={(e) => setZipCode(e.target.value)}
-                />
-            </Grid>
-           
-            </Grid>
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-            >
-                Sign Up
-            </Button>
-            </form>
-            </div>
-            </Container>
-            );
+		<Container component='main' maxWidth='xs'>
+			<CssBaseline />
+			<div>
+				<Avatar></Avatar>
+				<Typography component='h1' variant='h5'>
+					Sign up
+				</Typography>
+				<form onSubmit={handleSubmit} noValidate>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<TextField
+								autoComplete='off'
+								InputLabelProps={{ shrink: true }}
+								name='username'
+								required
+								fullWidth
+								id='username'
+								label='Username'
+								autoFocus
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete='off'
+								name='password'
+								InputLabelProps={{ shrink: true }}
+								required
+								fullWidth
+								id='password'
+								label='Password'
+								type='password'
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete='email'
+								name='email'
+								required
+								fullWidth
+								id='email'
+								label='Email'
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								autoComplete='street-address'
+								name='streetAddress'
+								required
+								fullWidth
+								id='streetAddress'
+								label='Street Address'
+								value={streetAddress}
+								onChange={(e) => setStreetAddress(e.target.value)}
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete='city'
+								name='city'
+								required
+								fullWidth
+								id='city'
+								label='City'
+								value={city}
+								onChange={(e) => setCity(e.target.value)}
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete='state'
+								name='state'
+								required
+								fullWidth
+								id='state'
+								label='State'
+								value={state}
+								onChange={(e) => setState(e.target.value)}
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete='zip-code'
+								name='zipCode'
+								required
+								fullWidth
+								id='zipCode'
+								label='Zip Code'
+								value={zipCode}
+								onChange={(e) => setZipCode(e.target.value)}
+							/>
+						</Grid>
+					</Grid>
+					<Button
+						type='submit'
+						fullWidth
+						variant='contained'
+						sx={{ mt: 3, mb: 2 }}>
+              <Link href="/">
+
+						Sign Up
+              </Link>
+					</Button>
+				</form>
+			</div>
+		</Container>
+	);
 };
             
