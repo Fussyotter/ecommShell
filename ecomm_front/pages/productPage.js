3159d56e271b980def431a22a3d698e059932895
ialoysious <isaacaloysious@gmail.com>
2023-05-03 17:00:06 -0700

    omg it works

--- ecomm_front/pages/productPage.js
+++ ecomm_front/pages/productPage.js
@@ -1,37 +1,41 @@
-import { useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
 
-export default function ProductPage({ product }) {
-	const [loading, setLoading] = useState(false);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+export default function CheckoutPage() {
+	const [sessionId, setSessionId] = useState('');
+
+	useEffect(() => {
+		const fetchSession = async () => {
+			const response = await fetch(`http://localhost:8000/api/checkout/bigshoe/`);
+			const data = await response.json();
+			setSessionId(data.session_id);
+		};
+
+		fetchSession();
+	}, []);
 
 	const handleClick = async () => {
-		setLoading(true);
-
-		// Fetch the Stripe checkout session ID from the Django backend
-		const response = await fetch(
-			`http://localhost:8000/api/create-checkout-session/prodTest`
-		);
-		const data = await response.json();
-		const sessionId = data.id;
-
-		// Redirect the user to the Stripe checkout page
-		const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+		const stripe = await stripePromise;
 		const { error } = await stripe.redirectToCheckout({ sessionId });
-
 		if (error) {
 			console.error(error);
-			setLoading(false);
 		}
 	};
 
 	return (
-		<div>
-			<h1>{product.title}</h1>
-			<p>{product.description}</p>
-			<p>Price: ${product.regular_price}</p>
-			<button disabled={loading} onClick={handleClick}>
-				{loading ? 'Loading...' : 'Buy Now'}
-			</button>
-		</div>
+		<>
+			<Head>
+				<title>Checkout</title>
+			</Head>
+			<div>
+				<h1>Checkout Page</h1>
+				<button onClick={handleClick} disabled={!sessionId}>
+					Pay with Stripe
+				</button>
+			</div>
+		</>
 	);
 }
