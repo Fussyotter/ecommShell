71193bf7bbfc3284c787b9ac89210d3cb9ec0058
ialoysious <isaacaloysious@gmail.com>
2023-05-04 14:55:19 -0700

    stripe almost implemented

--- ecomm_front/pages/productPage.js
+++ ecomm_front/pages/productPage.js
@@ -6,12 +6,18 @@ const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 export default function CheckoutPage() {
 	const [sessionId, setSessionId] = useState('');
+	const [products, setProducts] = useState('');
+	const [price, setPrice] = useState('');
+	const [image, setImage] = useState('');
 
 	useEffect(() => {
 		const fetchSession = async () => {
 			const response = await fetch(`http://localhost:8000/api/checkout/bigshoe/`);
 			const data = await response.json();
 			setSessionId(data.session_id);
+			setProducts(data.product)
+			setPrice(data.price);
+			setImage(data.image);
 		};
 
 		fetchSession();
@@ -31,6 +37,14 @@ export default function CheckoutPage() {
 				<title>Checkout</title>
 			</Head>
 			<div>
+				<h1>Products</h1>
+				<p>{products}</p>
+				<p>{price}</p>
+				<img
+					src={`http://localhost:8000${image}`}
+					alt='product'
+					style={{ width: '100%', maxWidth: '400px' }}
+				/>
 				<h1>Checkout Page</h1>
 				<button onClick={handleClick} disabled={!sessionId}>
 					Pay with Stripe
