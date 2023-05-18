f979ea24a46aad227bd64d9cf4247fc59a922577
ialoysious <isaacaloysious@gmail.com>
2023-05-18 14:45:41 -0700

    testing drop down instead of separate thing for cart

--- ecomm_front/components/cart.js
+++ ecomm_front/components/cart.js
@@ -1,12 +1,23 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { Csrf_context } from '../context/csrf_context';
 import { useAuth } from '@/context/auth_context';
-
+import {
+	Box,
+	List,
+	ListItem,
+	Collapse,
+	ListItemText,
+	IconButton,
+	Grid,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const Cart = () => {
-    const [cart, setCart] = useState({ items: [], total: 0 });
+	const [cart, setCart] = useState({ items: [], total: 0 });
+	const [open, setOpen] = useState(false);
 	const { csrfToken } = useContext(Csrf_context);
-	const {currentUser} = useAuth();
+	const { currentUser } = useAuth();
 
 	useEffect(() => {
 		// Fetch cart items from the backend when the component mounts
@@ -21,7 +32,7 @@ const Cart = () => {
 					'Content-Type': 'application/json',
 					'X-CSRFToken': csrfToken,
 				},
-                credentials: 'include',
+				credentials: 'include',
 			});
 			if (response.ok) {
 				const data = await response.json();
@@ -36,13 +47,34 @@ const Cart = () => {
 
 	return (
 		<div>
-			<h2>Your Cart</h2>
-			{cart.items.map((item) => (
-				<div key={item.id}>
-					<h3>{item.product.title}: {item.quantity}</h3>
-					<p>Price: ${item.product.regular_price}</p>
-				</div>
-			))}
+			<h2>Your Cart {cart.items.length}</h2>
+			<IconButton onClick={() => setOpen(!open)}>
+				<ExpandMoreIcon />
+			</IconButton>
+			<Box sx={{ mt: 1 }}>
+				<List>
+					<TransitionGroup>
+						{cart.items.map((item) => (
+							<CSSTransition key={item.id} timeout={500} classNames='item'>
+								<Collapse in={open}>
+									<ListItem>
+										<Grid container>
+											<Grid item xs={8}>
+												<p>
+													{item.product.title} : {item.quantity}
+												</p>
+											</Grid>
+											<Grid item xs={4}>
+												<p>${item.product.regular_price}</p>
+											</Grid>
+										</Grid>
+									</ListItem>
+								</Collapse>
+							</CSSTransition>
+						))}
+					</TransitionGroup>
+				</List>
+			</Box>
 			<h3>Total: ${cart.total}</h3>
 		</div>
 	);
