89c3b0aa3eebcdaf8dbc7ca0d77db52c1dc8e6ea
ialoysious <isaacaloysious@gmail.com>
2023-05-29 15:17:26 -0700

    touchups

--- ecomm_front/components/featuredProducts.js
+++ ecomm_front/components/featuredProducts.js
@@ -9,10 +9,10 @@ import {
 	Button,
 	Grid,
 	Typography,
+	Box,
 } from '@mui/material';
 import Link from 'next/link';
 const PAGE_SIZE = 10;
-
 
 export default function FeaturedProducts() {
 	const { csrfToken } = useContext(Csrf_context);
@@ -22,23 +22,22 @@ export default function FeaturedProducts() {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [totalPages, setTotalPages] = useState(1);
 
-const fetchProducts = (page, searchTerm) => {
-	let endpoint = `http://localhost:8000/api/?page=${page}`;
-	if (searchTerm) {
-		endpoint += `&search=${searchTerm}`;
-	}
-	fetch(endpoint)
-		.then((response) => response.json())
-		.then((data) => {
-			setProducts(data.results);
-			setTotalPages(Math.ceil(data.count / PAGE_SIZE)); // calculate total pages
-		})
-		.catch(console.error);
-};
+	const fetchProducts = (page, searchTerm) => {
+		let endpoint = `http://localhost:8000/api/?page=${page}`;
+		if (searchTerm) {
+			endpoint += `&search=${searchTerm}`;
+		}
+		fetch(endpoint)
+			.then((response) => response.json())
+			.then((data) => {
+				setProducts(data.results);
+				setTotalPages(Math.ceil(data.count / PAGE_SIZE)); // calculate total pages
+			})
+			.catch(console.error);
+	};
 
 	const addToCart = (product_id, quantity) => {
 		console.log('addToCart called', product_id, quantity);
-
 		fetch(`http://localhost:8000/cart/add/${product_id}/`, {
 			method: 'POST',
 			headers: {
@@ -46,7 +45,6 @@ const fetchProducts = (page, searchTerm) => {
 				'X-CSRFToken': csrfToken,
 			},
 			credentials: 'include',
-
 			body: JSON.stringify({
 				product_id: product_id,
 				quantity: quantity || 1,
@@ -62,67 +60,112 @@ const fetchProducts = (page, searchTerm) => {
 				// TODO: Show an error message
 			});
 	};
-  useEffect(() => {
+	useEffect(() => {
 		fetchProducts(currentPage, searchTerm);
 	}, [currentPage, searchTerm]);
 	return (
 		<div>
 			<h1>Featured Products</h1>
-			<Grid container spacing={2}>
-				{products.map((product) => (
-					<Grid key={product.id} item xs={12} sm={6} md={4} lg={2}>
-						<Card
-							raised
-							sx={{
-								height: '100%',
-								maxWidth: 280,
-								boxShadow: 15,
-								flexDirection: 'column',
-								margin: '0 auto',
-								padding: '1em 1em 0 1em',
-							}}>
-							{product.product_image.length > 0 &&
-								product.product_image[0].image && (
-									<CardMedia
-										component='img'
-										height={150}
-										alt={product.title}
-										image={product.product_image[0].image}
-									/>
-								)}
-							<CardContent sx={{ flexGrow: 1 }}>
-								<Link href={`/${product.slug}`}>
-									<Typography gutterBottom variant='h5' component='h2'>
-										{product.title}
+			<Grid container spacing={1}>
+				{products.map((product, index) =>
+					index % 3 === 2 ? (
+						<Grid key={product.id} item xs={12} sm={6} md={4} lg={2}>
+							<Card
+								raised
+								sx={{
+									height: '100%',
+									maxWidth: 345, // Increase the max width for every 3rd item
+									boxShadow: 20,
+									flexDirection: 'column',
+									margin: '1em auto',
+									padding: '1em .5em 0 .5em',
+								}}>
+								{product.product_image.length > 0 &&
+									product.product_image[0].image && (
+										<CardMedia
+											component='img'
+											height={150}
+											alt={product.title}
+											image={product.product_image[0].image}
+										/>
+									)}
+								<CardContent sx={{ flexGrow: 1 }}>
+									<Link href={`/${product.slug}`}>
+										<Typography gutterBottom variant='h5' component='h2'>
+											{product.title}
+										</Typography>
+									</Link>
+									<Typography variant='body2' color='text.secondary'>
+										{product.description}
+										{product.product_image.image}
 									</Typography>
-								</Link>
-								<Typography variant='body2' color='text.secondary'>
-									{product.description}
-									{product.product_image.image}
-								</Typography>
-								<p>${product.regular_price}</p>
-							</CardContent>
-							<CardActions>
-								<Button onClick={() => addToCart(product.id)}>
-									Add to cart
-								</Button>
-							</CardActions>
-						</Card>
-					</Grid>
-				))}
+									<p>${product.regular_price}</p>
+								</CardContent>
+								<CardActions>
+									<Button onClick={() => addToCart(product.id)}>
+										Add to cart
+									</Button>
+								</CardActions>
+							</Card>
+						</Grid>
+					) : (
+						<Grid key={product.id} item xs={12} sm={6} md={4} lg={2}>
+							<Card
+								raised
+								sx={{
+									height: '100%',
+									maxWidth: 280,
+									boxShadow: 20,
+									flexDirection: 'column',
+									margin: '1em auto',
+									padding: '1em .5em 0 .5em',
+								}}>
+								{product.product_image.length > 0 &&
+									product.product_image[0].image && (
+										<CardMedia
+											component='img'
+											height={150}
+											alt={product.title}
+											image={product.product_image[0].image}
+										/>
+									)}
+								<CardContent sx={{ flexGrow: 1 }}>
+									<Link href={`/${product.slug}`}>
+										<Typography gutterBottom variant='h5' component='h2'>
+											{product.title}
+										</Typography>
+									</Link>
+									<Typography variant='body2' color='text.secondary'>
+										{product.description}
+										{product.product_image.image}
+									</Typography>
+									<p>${product.regular_price}</p>
+								</CardContent>
+								<CardActions>
+									<Button onClick={() => addToCart(product.id)}>
+										Add to cart
+									</Button>
+								</CardActions>
+							</Card>
+						</Grid>
+					)
+				)}
 			</Grid>
-			<div>
-				<button
+			<Box display='flex' justifyContent='center' mt={3}>
+				<Button
+					variant='contained'
 					onClick={() => setCurrentPage((oldPage) => Math.max(oldPage - 1, 1))}>
 					Previous Page
-				</button>
-				<button
+				</Button>
+				<Button
+					variant='contained'
 					onClick={() =>
 						setCurrentPage((oldPage) => Math.min(oldPage + 1, totalPages))
-					}>
+					}
+					style={{ marginLeft: '10px' }}>
 					Next Page
-				</button>
-			</div>
+				</Button>
+			</Box>
 		</div>
 	);
 }
--- ecomm_front/components/header.js
+++ ecomm_front/components/header.js
@@ -12,6 +12,8 @@ import {
 } from '@mui/material';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
+
+
 import { useAuth } from '@/context/auth_context';
 import { Csrf_context } from '@/context/csrf_context';
 import { Search_context } from '@/context/search_context';
@@ -21,7 +23,7 @@ const Header = () => {
 	const { currentUser, setCurrentUser } = useAuth();
 	const { csrfToken } = React.useContext(Csrf_context);
 	const { setSearchTerm } = React.useContext(Search_context);
-	const categories = ['test', 'hats', 'shirts','shoes','soaps']; // These are dummy categories that I'm using only for the purpose of this demo
+	const categories = ['test', 'hats', 'shirts', 'shoes', 'soaps']; // These are dummy categories that I'm using only for the purpose of this demo
 	const [inputValue, setInputValue] = React.useState('');
 
 	function handleLogout() {
@@ -57,13 +59,21 @@ const Header = () => {
 			<Toolbar
 				sx={{
 					display: 'flex',
+					flexDirection: 'column',
 					alignItems: 'center',
 					justifyContent: 'space-between',
 					paddingBottom: 3,
 					paddingTop: 1,
 					borderBottom: '2px solid black',
 				}}>
-				<Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1, px: 4 }}>
+				<Box
+					sx={{
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'space-between',
+						width: '100%',
+						px: 4,
+					}}>
 					<h2>Ecommerce Shell</h2>
 					<form onSubmit={handleSearchSubmit} style={{ flexGrow: 1 }}>
 						<TextField
@@ -83,13 +93,70 @@ const Header = () => {
 							}}
 						/>
 					</form>
-				</Box>
+					<Box
+						sx={{
+							display: 'flex',
+							flexDirection: 'row',
+							alignItems: 'flex-end',
+							justifyContent: 'flex-end', 
+							px: 4,
+						}}>
+						 {currentUser ? (
+    <Box 
+      sx={{ 
+        display: 'flex', 
+        alignItems: 'center' 
+      }}
+    >
+      <Cart />
+      <Typography variant='body2' sx={{ marginLeft: '1rem' }}>
+        Welcome, {currentUser}
+      </Typography>
+      <Button
+        size='small'
+        variant='contained'
+        onClick={handleLogout}
+        sx={{ m: 1 }}
+      >
+        Logout
+      </Button>
+    </Box>
+  ) : (
+    <Box 
+      sx={{ 
+        display: 'flex', 
+        alignItems: 'center' 
+      }}
+    >
+      <Link href='/login' variant='body2'>
+        Login
+      </Link>
+    </Box>
+  )}
+</Box>
+
+<Box
+  sx={{
+    display: 'flex',
+    justifyContent: 'flex-end', 
+    px: 4,
+  }}
+>
+  <Link href='/signup' variant='body2'>
+    Register
+  </Link>
+</Box>
+</Box>  	
+
 				<Box
 					sx={{
 						display: 'flex',
-						alignItems: 'center',
-						flexDirection: 'row', 
+						alignItems: 'center', // Changed from 'flex-end' to 'center' for better alignment
+						flexDirection: 'row', // Changed from 'column' to 'row' to align items horizontally
 						p: 1,
+						width: '100%',
+						justifyContent: 'center', // Added to center the category buttons
+						flexWrap: 'wrap', // Added in case there are many categories and they need to wrap to a new line
 					}}>
 					{categories.map((category) => (
 						<Button
@@ -99,38 +166,6 @@ const Header = () => {
 							{category}
 						</Button>
 					))}
-					<Box
-						sx={{
-							display: 'flex',
-							flexDirection: 'column',
-							alignItems: 'flex-end',
-							p: 1,
-						}}>
-						{currentUser ? (
-							<div style={{ display: 'flex', alignItems: 'center' }}>
-								<Cart />
-								<div>
-									<Typography variant='body2' sx={{ marginLeft: '1rem' }}>
-										Welcome, {currentUser}
-									</Typography>
-								</div>
-								<Button
-									size='small'
-									variant='contained'
-									onClick={handleLogout}
-									sx={{ m: 1 }}>
-									Logout
-								</Button>{' '}
-							</div>
-						) : (
-							<Link href='/login' variant='body2'>
-								{'Login'}
-							</Link>
-						)}
-						<Link href='/signup' variant='body2'>
-							{'Register'}
-						</Link>
-					</Box>
 				</Box>
 			</Toolbar>
 		</AppBar>
