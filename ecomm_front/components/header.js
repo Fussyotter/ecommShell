699db5e3c33a295ed6b6dfa2e417958007eb880f
ialoysious <isaacaloysious@gmail.com>
2023-05-29 14:10:28 -0700

    categories added to search

--- ecomm_front/components/header.js
+++ ecomm_front/components/header.js
@@ -14,12 +14,16 @@ import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
 import { useAuth } from '@/context/auth_context';
 import { Csrf_context } from '@/context/csrf_context';
-import { Search_context} from '@/context/search_context';
+import { Search_context } from '@/context/search_context';
 import Cart from './cart';
 
 const Header = () => {
 	const { currentUser, setCurrentUser } = useAuth();
 	const { csrfToken } = React.useContext(Csrf_context);
+	const { setSearchTerm } = React.useContext(Search_context);
+	const categories = ['test', 'hats', 'shirts','shoes','soaps']; // These are dummy categories that I'm using only for the purpose of this demo
+	const [inputValue, setInputValue] = React.useState('');
+
 	function handleLogout() {
 		fetch('http://localhost:8000/users/logout/', {
 			method: 'POST',
@@ -41,6 +45,13 @@ const Header = () => {
 				console.error(err);
 			});
 	}
+	const handleChange = (event) => {
+		setInputValue(event.target.value);
+	};
+	const handleSearchSubmit = (event) => {
+		event.preventDefault();
+		setSearchTerm(inputValue);
+	};
 	return (
 		<AppBar position='sticky'>
 			<Toolbar
@@ -54,53 +65,72 @@ const Header = () => {
 				}}>
 				<Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1, px: 4 }}>
 					<h2>Ecommerce Shell</h2>
-					<TextField
-						fullWidth
-						variant='outlined'
-						placeholder='Search'
-						sx={{ bgcolor: 'white', py: 0.5, borderRadius: 1, ml: 2 }}
-						InputProps={{
-							endAdornment: (
-								<InputAdornment position='end'>
-									<IconButton type='submit'>
-										<SearchIcon />
-									</IconButton>
-								</InputAdornment>
-							),
-						}}
-					/>
+					<form onSubmit={handleSearchSubmit} style={{ flexGrow: 1 }}>
+						<TextField
+							fullWidth
+							variant='outlined'
+							placeholder='Search'
+							sx={{ bgcolor: 'white', py: 0.5, borderRadius: 1, ml: 2 }}
+							onChange={handleChange}
+							InputProps={{
+								endAdornment: (
+									<InputAdornment position='end'>
+										<IconButton type='submit'>
+											<SearchIcon />
+										</IconButton>
+									</InputAdornment>
+								),
+							}}
+						/>
+					</form>
 				</Box>
 				<Box
 					sx={{
 						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'flex-end',
+						alignItems: 'center',
+						flexDirection: 'row', 
 						p: 1,
 					}}>
-					{currentUser ? (
-						<div style={{ display: 'flex', alignItems: 'center' }}>
-							<Cart />
-							<div>
-								<Typography variant='body2' sx={{ marginLeft: '1rem' }}>
-									Welcome, {currentUser}
-								</Typography>
+					{categories.map((category) => (
+						<Button
+							size='small'
+							variant='contained'
+							onClick={() => setSearchTerm(category)}>
+							{category}
+						</Button>
+					))}
+					<Box
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'flex-end',
+							p: 1,
+						}}>
+						{currentUser ? (
+							<div style={{ display: 'flex', alignItems: 'center' }}>
+								<Cart />
+								<div>
+									<Typography variant='body2' sx={{ marginLeft: '1rem' }}>
+										Welcome, {currentUser}
+									</Typography>
+								</div>
+								<Button
+									size='small'
+									variant='contained'
+									onClick={handleLogout}
+									sx={{ m: 1 }}>
+									Logout
+								</Button>{' '}
 							</div>
-							<Button
-								size='small'
-								variant='contained'
-								onClick={handleLogout}
-								sx={{ m: 1 }}>
-								Logout
-							</Button>{' '}
-						</div>
-					) : (
-						<Link href='/login' variant='body2'>
-							{'Login'}
+						) : (
+							<Link href='/login' variant='body2'>
+								{'Login'}
+							</Link>
+						)}
+						<Link href='/signup' variant='body2'>
+							{'Register'}
 						</Link>
-					)}
-					<Link href='/signup' variant='body2'>
-						{'Register'}
-					</Link>
+					</Box>
 				</Box>
 			</Toolbar>
 		</AppBar>
